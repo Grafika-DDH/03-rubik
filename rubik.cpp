5eb48439d5701013100474d80f6c36a205dfdc4f
Ahmad N. F <fahmadnaufal@gmail.com>
2016-04-25 14:46:22 +0700

    rotation animation 6/6 (complete)

--- rubik.cpp
+++ rubik.cpp
@@ -714,6 +714,71 @@ void draw_cube(float angle, float viewX, float viewY, float viewZ, int rotation)
             // rotated left cube
             draw_front_side_cube();
             break;
+
+        // Back rotations
+        case 6:
+            draw_front_side_cube();
+
+            // top side cube
+            polygon(top[1][0],48,49,50,51);
+            polygon(top[1][1],52,53,54,55);
+            polygon(top[1][2],56,57,58,59);
+            polygon(top[2][0],60,61,62,63);
+            polygon(top[2][1],64,65,66,67);
+            polygon(top[2][2],68,69,70,71);
+
+            // bottom side cube
+            polygon(bottom[0][0],108,109,110,111);
+            polygon(bottom[0][1],112,113,114,115);
+            polygon(bottom[0][2],116,117,118,119);
+            polygon(bottom[1][0],120,121,122,123);
+            polygon(bottom[1][1],124,125,126,127);
+            polygon(bottom[1][2],128,129,130,131);
+
+            // left side cube
+            polygon(left[0][1],148,149,150,151);
+            polygon(left[0][2],152,153,154,155);
+            polygon(left[1][1],160,161,162,163);
+            polygon(left[1][2],164,165,166,167);
+            polygon(left[2][1],172,173,174,175);
+            polygon(left[2][2],176,177,178,179);
+
+            // right side cube
+            polygon(right[0][0],180,181,182,183);
+            polygon(right[0][1],184,185,186,187);
+            polygon(right[1][0],192,193,194,195);
+            polygon(right[1][1],196,197,198,199);
+            polygon(right[2][0],204,205,206,207);
+            polygon(right[2][1],208,209,210,211);
+
+            glPushMatrix();
+            glColor3fv(color[0]);
+            glPopMatrix();
+            glRotatef(-angle, 0.f, 0.f, 1.f);
+
+            // rotated top cube
+            polygon(top[0][0],36,37,38,39);
+            polygon(top[0][1],40,41,42,43);
+            polygon(top[0][2],44,45,46,47);
+
+            // rotated bottom cube
+            polygon(bottom[2][0],132,133,134,135);
+            polygon(bottom[2][1],136,137,138,139);
+            polygon(bottom[2][2],140,141,142,143);
+
+            // rotated left cube
+            polygon(left[0][0],144,145,146,147);
+            polygon(left[1][0],156,157,158,159);
+            polygon(left[2][0],168,169,170,171);
+
+            // rotated right cube
+            polygon(right[0][2],188,189,190,191);
+            polygon(right[1][2],200,201,202,203);
+            polygon(right[2][2],212,213,214,215);
+
+            // rotated left cube
+            draw_back_side_cube();
+            break;
     }
 
 }
